34b354412f11fb6961763c9f9a21a328e5ddb8a6
QUDDUS <qudduslarek@proton.me>
2026-04-04 17:01:15 +0500

    add: "header and layout for code preview"

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
-import { ToggleThemeButton } from "@/components/custom/toogle-theme";
-import Image from "next/image";
+"use client"
+
+import { Header } from "@/components/custom/header";
+import { ViewerLayout } from "@/components/layouts/tabs.layout";
 
 export default function Home() {
   return (
-    <>
-    
-    </>
+    <div className="h-svh w-full">
+      <Header />
+      <ViewerLayout />
+    </div>
   );
 }
