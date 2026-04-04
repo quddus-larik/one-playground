12b4c45352b7fbfb7f2b1eac7851e3eecb8c79ea
QUDDUS <qudduslarek@proton.me>
2026-04-04 17:04:13 +0500

    feat: "global states management using zustand for viewer State"

---
+++ stores/tabs.state.ts
@@ -0,0 +1,15 @@
+import { create } from 'zustand';
+
+interface ViewerPayload {
+  viewerState: string | '';
+}
+
+type ViewerStateInterface = {
+  viewerState: string | '';
+  setViewerState: (payload: ViewerPayload) => void;
+};
+
+export const useViewer = create<ViewerStateInterface>((set) => ({
+  viewerState: 'both',  // Initial value
+  setViewerState: (payload) => set(payload),
+}));
